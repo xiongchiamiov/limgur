72e4d4b5ef0a399284e3defff51914cba6af07b3
Danny Tatom <dannytatom@gmail.com>
2010-01-02 00:16:13 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# limgur
+
+CLI to Imgur, letting you upload and delete images.
+If you've got scrot installed, you can also take a screenshot and upload it in one command.
+
+## Usage
+
+Usage: limgur [options]
+    -u, --upload                     Upload via image or URL
+    -d, --delete                     Delete an iamge via hash
+    -s, --scrot                      Take a screenshot then upload it
+    -h, --help                       Show this message
+    -v, --version                    Show version
+
+## Copyright
+
+Copyright (c) 2009 Danny Tatom. See LICENSE for details.
---
+++ lib/limgur.rb
@@ -0,0 +1,61 @@
+require 'open-uri'
+require 'curb'
+require 'crack/json'
+
+class Limgur
+  def initialize key
+    @key = key
+  end
+
+  def upload image
+    begin
+      if URI.parse(image).scheme
+        c = Curl::Easy.new 'http://imgur.com/api/upload.json'
+        c.http_post Curl::PostField.content('key', @key),
+                    Curl::PostField.content('image', image)
+        response = Crack::JSON.parse c.body_str
+      else
+        c = Curl::Easy.new 'http://imgur.com/api/upload.json'
+        c.multipart_form_post = true
+        c.http_post Curl::PostField.content('key', @key),
+                    Curl::PostField.file('image', image)
+        response = Crack::JSON.parse c.body_str
+      end
+
+      if response['error']
+        response['error']['error_msg']
+      else
+        "Image was uploaded successfully!\n\n" \
+        "Imgur page:     #{response['rsp']['image']['imgur_page']}\n" \
+        "Original image: #{response['rsp']['image']['original_image']}\n" \
+        "Delete hash:    #{response['rsp']['image']['delete_hash']}\n"
+      end
+    rescue
+      Curl::Err::ReadError
+      'Please provide a valid image.'
+    end
+  end
+
+  def delete hash
+    c = Curl::Easy.new "http://imgur.com/api/delete/#{hash}.json"
+    c.http_get
+    response = Crack::JSON.parse c.body_str
+
+    if response['error']
+      response['error']['error_msg']
+    else
+      'Image was successfully deleted!'
+    end
+  end
+
+  def scrot
+    filename = Time.now.to_s.gsub(' ', '').gsub(':', '') + '.png'
+
+    system 'scrot ' + filename
+    upload File.expand_path filename
+  end
+
+  def self.version
+    '1.0.0'
+  end
+end
